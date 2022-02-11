37e22dded7e9b608bb91e69e59cccb8aa60e4780
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-02-11 15:26:27 +0100

    added buttons that list the new transaction widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_personal_expenses/widgets/user_transaction.dart';
+import 'package:flutter_personal_expenses/models/transaction.dart';
+import 'package:flutter_personal_expenses/widgets/new_transaction.dart';
+import 'package:flutter_personal_expenses/widgets/transaction_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,28 +19,80 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> userTransactions = [
+    Transaction(
+        amomunt: 60, date: DateTime.now(), id: 't1', title: 'New Shoes'),
+    Transaction(
+        amomunt: 40, date: DateTime.now(), id: 't1', title: 'Weekly Groceries'),
+    Transaction(
+        amomunt: 30, date: DateTime.now(), id: 't1', title: 'New Shoes2'),
+    Transaction(
+        amomunt: 57, date: DateTime.now(), id: 't1', title: 'New Shoes3'),
+  ];
+
+  void addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        amomunt: amount,
+        date: DateTime.now(),
+        id: DateTime.now().toString(),
+        title: title);
+
+    setState(() {
+      userTransactions.add(newTx);
+    });
+  }
+
+  void startAddNewTransaction(ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (bCtx) {
+          return GestureDetector(
+              onTap: () {},
+              behavior: HitTestBehavior.opaque,
+              child: NewTransaction(addNewTransaction: addNewTransaction));
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter App"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                startAddNewTransaction(context);
+              },
+              icon: const Icon(Icons.add))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Card(
+          children: [
+            const Card(
               color: Colors.blue,
               child: Text("CHART"),
               elevation: 5,
             ),
-            UserTransaction(),
+            TransactionList(userTransactions: userTransactions)
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            startAddNewTransaction(context);
+          },
+          child: const Icon(Icons.add)),
     );
   }
 }
